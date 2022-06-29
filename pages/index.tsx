d469defe351fff2344ad9e40f9dea6dca913b46d
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 01:55:02 +0900

    :memo: #4.7 | Modifiers for Forms / 4
    - required, placeholder, invalid

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,19 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 bg-blue-500 p-5">
-      <input type="text" required placeholder="Username" />
-      <input type="password" required placeholder="Password" />
+    <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
+      <input
+        type="text"
+        required
+        placeholder="Username"
+        className="required:border-2 border-yellow-500 placeholder-shown:bg-teal-500 placeholder:text-red-500"
+      />
+      <input
+        type="password"
+        required
+        placeholder="Password"
+        className="invalid:bg-red-500"
+      />
       <input type="submit" value="login" className="bg-white" />
     </form>
   );
