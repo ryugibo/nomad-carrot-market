a6daf4dba2b50457b7f95e6711221b56a1b1be65
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 01:58:48 +0900

    :memo: #4.7 | Modifiers for Forms / 7
    - form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,14 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
+    <form className="flex flex-col space-y-2 p-5">
       <input
         type="text"
         required
         placeholder="Username"
-        className="required:bg-yellow-500 invalid:bg-red-500 valid:bg-teal-500"
+        className="border p-1 border-gray-400 rounded"
       />
-      <input type="password" required placeholder="Password" className="" />
-      <input type="submit" value="login" className="bg-white" />
+      <input type="submit" value="Login" className="bg-white" />
     </form>
   );
 };
