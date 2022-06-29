959e58b79d05c326f981911c5d7cc9dda29b04ef
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 02:20:27 +0900

    :memo: #4.8 | More Modifiers / 2
    - first-letter

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,12 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <details className="select-none open:text-white open:bg-indigo-400">
-        <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span>김치</span>
-      </details>
-
-      <ul className="list-decimal marker:text-teal-500">
-        <li>hi</li>
-        <li>hi</li>
-        <li>hi</li>
-      </ul>
-
-      <input
-        type="file"
-        className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400 bg-blue-200"
-      />
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, illo
+        ad? Consequatur assumenda culpa, enim similique atque quae sit esse quas
+        reiciendis natus repudiandae debitis, excepturi repellendus harum! Illo,
+        quaerat?
+      </p>
     </div>
   );
 };
