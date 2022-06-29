53df72b2fca932ad210ceab3a55b9975693cb720
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 01:55:49 +0900

    :memo: #4.7 | Modifiers for Forms / 5
    - disabled

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,11 @@ const Home: NextPage = () => {
     <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
       <input
         type="text"
-        required
+        disabled
         placeholder="Username"
-        className="required:border-2 border-yellow-500 placeholder-shown:bg-teal-500 placeholder:text-red-500"
+        className="disabled:opacity-0"
       />
-      <input
-        type="password"
-        required
-        placeholder="Password"
-        className="invalid:bg-red-500"
-      />
+      <input type="password" required placeholder="Password" className="" />
       <input type="submit" value="login" className="bg-white" />
     </form>
   );
