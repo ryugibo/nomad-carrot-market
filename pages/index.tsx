c7794e5e53e18a64758677c3137fe8723d98c358
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 16:02:29 +0900

    :wrench: prettier tailwindcss 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,22 @@ const Home: NextPage = () => {
       {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
         <div
           key={i}
-          className="flex border-b pb-4 px-4 cursor-pointer justify-between"
+          className="flex cursor-pointer justify-between border-b px-4 pb-4"
         >
           <div className="flex space-x-4">
-            <div className="w-20 h-20 bg-gray-400 rounded-md" />
-            <div className="pt-2 flex flex-col">
+            <div className="h-20 w-20 rounded-md bg-gray-400" />
+            <div className="flex flex-col pt-2">
               <h3 className="text-sm font-semibold text-gray-900">
                 New iPhone 14
               </h3>
               <span className="text-xs text-gray-500">Black</span>
-              <span className="font-semibold mt-1 text-gray-900">$95</span>
+              <span className="mt-1 font-semibold text-gray-900">$95</span>
             </div>
           </div>
           <div className="flex items-end justify-end space-x-2">
-            <div className="flex items-center text-sm text-gray-600 space-x-0.5">
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
               <svg
-                className="w-4 h-4"
+                className="h-4 w-4"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -36,9 +36,9 @@ const Home: NextPage = () => {
               </svg>
               <span>1</span>
             </div>
-            <div className="flex items-center text-sm text-gray-600 space-x-0.5">
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
               <svg
-                className="w-4 h-4"
+                className="h-4 w-4"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
-      <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-4 text-white shadow-xl hover:bg-orange-500 cursor-pointer transition-colors">
+      <button className="fixed bottom-24 right-5 cursor-pointer rounded-full bg-orange-400 p-4 text-white shadow-xl transition-colors hover:bg-orange-500">
         <svg
           className="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
