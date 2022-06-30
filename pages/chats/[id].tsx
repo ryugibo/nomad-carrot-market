f2ebcc051602fcefe976b955a97cefb8b8b188f1
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-30 22:49:48 +0900

    :memo: #5.9 | Chat Detail / 1
    - html

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div className="py-10 px-4">
+      <div>
+        <div />
+        <div>Hi how much are you selling them for?</div>
+      </div>
+      <div>
+        <div />
+        <div>I want 20,000</div>
+      </div>
+      <div>
+        <div />
+        <div>미쳤어</div>
+      </div>
+      <div>
+        <div>
+          <input type="text" />
+          <div>
+            <span>&rarr;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;
